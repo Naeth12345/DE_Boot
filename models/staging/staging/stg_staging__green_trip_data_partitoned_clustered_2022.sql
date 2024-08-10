d1d201880b88f5d01ed0f58cbf0eaad5611fa3e1
Dimitris Katsimpris <dimitriskatsimbris@gmail.com>
2024-08-10 18:22:39 +0000

    h

---
+++ models/staging/staging/stg_staging__green_trip_data_partitoned_clustered_2022.sql
@@ -0,0 +1,38 @@
+with 
+
+source as (
+
+    select * from {{ source('staging', 'green_trip_data_partitoned_clustered_2022') }}
+
+),
+
+renamed as (
+
+    select
+        vendorid,
+        lpep_pickup_datetime,
+        lpep_dropoff_datetime,
+        store_and_fwd_flag,
+        ratecodeid,
+        pulocationid,
+        dolocationid,
+        passenger_count,
+        trip_distance,
+        fare_amount,
+        extra,
+        mta_tax,
+        tip_amount,
+        tolls_amount,
+        ehail_fee,
+        improvement_surcharge,
+        total_amount,
+        payment_type,
+        trip_type,
+        congestion_surcharge,
+        month
+
+    from source
+
+)
+
+select * from renamed
